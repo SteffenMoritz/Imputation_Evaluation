ad8ca2f3884698f765de490a87830caae3ee3f32
Marouane Seffal <marouane.seffal@statcan.gc.ca>
2025-05-21 09:02:36 -0400

    remove purrr usage and add get iteration and save scenario

--- R/testscript_MS.R
+++ R/testscript_MS.R
@@ -2,10 +2,14 @@ library(here)
 library(mice)
 library(arrow)
 
-source(here("R", "simulate_missing_data.R")) 
+source(here("R", "a_generate_missingness_scenario.R")) 
+source(here("R", "scenario_get_simulated_dataset.R")) 
+source(here("R", "scenario_save.R")) 
 
 data <- data.frame(read_parquet(here('simulated_data', 'simulated_complete_data.parq')))
-head(data)
+# No factor columns in data_bis
+data_bis <- subset(data, select = -c(employees_cat, small_business))
+head(data_bis)
 
 ### Function Definitions ####
 
@@ -28,23 +32,24 @@ miss_missMethods <- function(data, prop, method='delete_MCAR', ...){
 }
 
 ### Procudure ####
-#class simulations
-md_sims  <- simulate_missing_data(x=data, unit_id='business_number',
-                                  miss_func=miss_missMethods, method='delete_MNAR_rank', cols_mis=11, 
-                                  n = 100)
+#class missingness_scenario
+gen_miss  <- a_generate_missingness_scenario(x=data_bis, unit_id='business_number',
+                                                 miss_func=miss_missMethods, method='delete_MNAR_rank', cols_mis=11, 
+                                                 n = 10)
+print(gen_miss)
+tail(gen_miss$miss_iterations, 25)
+table(gen_miss$miss_iterations$iter)
 
-tail(md_sims$miss_trials, 25)
+# Get particular iteration(s)
+iter5 <- scenario_get_iteration(gen_miss, 5)
+head(iter5$scenario_iteration, 25)
+table(iter5$scenario_iteration$iter)
 
-save_simulations(md_sims, file =)
-get_simulation(number)
-
-print(md_sims)
-
-simulate_missing_data()
-
-# save(obj)
-# simulations_save(md_sims, file =)
-# simulations_get(class simulation)
-# simulations_print
-# simulations_plot
+# Save
+# gc()
+scenario_save(gen_miss, save_path = here("simulated_data", "out", "one_iteration_test.parq"),
+              optimized=TRUE)
+df <- data.frame(read_parquet(here("simulated_data", "out", "one_iteration_test.parq")))
+table(df$iter)
+head(df)
 
